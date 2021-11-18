e04eb4b604832d6afed42998ba3eceb1c50ac728
ninhTKieen <ninhtrungkien2811@gmail.com>
2021-11-18 11:50:54 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
-import TaskList from './components/TaskList';
+import React from "react";
+import "./App.css";
+
+import TaskList from "./components/TaskList";
 function App() {
   return (
     <div className="App">
